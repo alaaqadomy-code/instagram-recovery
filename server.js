85deef0492eab2e076fa9c82e91c27cdc43f404f
alaaqadomy-code <alaaqadomy-code@users.noreply.github.com>
2026-09-25 16:17:10 +0300

    Add a Passenger entrypoint so Next.js can run on the host.

---
+++ server.js
@@ -0,0 +1,14 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const hostname = "127.0.0.1";
+const app = next({ dev: false, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res, parse(req.url, true));
+  }).listen(port, hostname);
+});
